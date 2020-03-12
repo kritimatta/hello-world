264533fb3e5c5fa2903a21cdaf3cd531caec2a69
kritimatta <kritimatta@yahoo.in>
2020-03-11 21:09:52 -0500

    Test

---
+++ app/src/main/java/com/example/attempt3000/easygift.java
@@ -0,0 +1,14 @@
+package com.example.attempt3000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class easygift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easygift);
+    }
+}
